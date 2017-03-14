aebc32a6045fab6f1828b70bf61a95a81c52a462
Andreas Hruschka <hruschka@mecodia.de>
2017-03-14 14:40:09 +0100

    AZB-67 changed pass_word to password.

--- cdplib/misc/create_list.py
+++ cdplib/misc/create_list.py
@@ -9,10 +9,10 @@ with open("10-million-combos.txt", "r") as password_file:
             break
         lines = line.split()
         try:
-            pass_word = lines[1]
+            password = lines[1]
         except:
-            pass_word = lines[0]
-        password_list.append(pass_word.decode('cp1252').encode('utf-8'))
+            password = lines[0]
+        password_list.append(password.decode('cp1252').encode('utf-8'))
 
 with open("common.coffee","w") as passwords:
     passwords.write("module.exports.passwords.common = ")
