2b6f0a960a3b5f3c41b8f6bc26cf172ede5e47dc
Andreas Hruschka <hruschka@mecodia.de>
2017-03-14 14:33:26 +0100

    AZB-67 improved variable names.

--- cdplib/misc/create_list.py
+++ cdplib/misc/create_list.py
@@ -1,6 +1,6 @@
 import json
 
-passwordList =[]
+password_list =[]
 with open("10-million-combos.txt", "r") as password_file:
 
     for idx,line in enumerate(password_file):
@@ -9,11 +9,11 @@ with open("10-million-combos.txt", "r") as password_file:
             break
         lines = line.split()
         try:
-            passs = lines[1]
+            pass_word = lines[1]
         except:
-            passs = lines[0]
-        passwordList.append(passs.decode('cp1252').encode('utf-8'))
+            pass_word = lines[0]
+        password_list.append(pass_word.decode('cp1252').encode('utf-8'))
 
 with open("common.coffee","w") as passwords:
     passwords.write("module.exports.passwords.common = ")
-    json.dump(passwordList,passwords)
+    json.dump(password_list,passwords)
